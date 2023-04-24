9b85bd66cd8349e225dbedeb3c744228ccc7086c
Luke <lukestrazzera@gmail.com>
2023-04-24 16:46:05 -0400

    Fixed 'seed' issue.

---
+++ ContactManagerStarter/Migrations/20230424204423_CLIENTDATABASE.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactManagerStarter.Migrations
+{
+    /// <inheritdoc />
+    public partial class CLIENTDATABASE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
